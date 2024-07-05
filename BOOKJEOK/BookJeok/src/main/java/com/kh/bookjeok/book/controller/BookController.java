2f5ce0031c3767b03ef91116aada7d10cca50e06
hantaeyeok <160584121+hantaeyeok@users.noreply.github.com>
2024-07-05 17:25:33 +0900

    gitTest-태역
    git Test입니다.

---
+++ BOOKJEOK/BookJeok/src/main/java/com/kh/bookjeok/book/controller/BookController.java
@@ -0,0 +1,12 @@
+package com.kh.bookjeok.book.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+
+	public void gitTest() {
+		System.out.println("gitTest입니다.");
+	}
+	
+}
