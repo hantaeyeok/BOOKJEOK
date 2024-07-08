fbf7e0c4467ca9b759315f61fb237320f50027c8
hantaeyeok <160584121+hantaeyeok@users.noreply.github.com>
2024-07-08 15:45:21 +0900

    insertBook.jsp BookApiController 복구- 태역

--- BOOKJEOK/BookJeok/src/main/java/com/kh/bookjeok/book/controller/BookController.java
+++ BOOKJEOK/BookJeok/src/main/java/com/kh/bookjeok/book/controller/BookController.java
@@ -1,44 +1,16 @@
 package com.kh.bookjeok.book.controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@RestController
+@Controller
 @RequestMapping("book")
 public class BookController {
-
-	@GetMapping
-	public String searchAladinAPI(String keyword) {
-		
-		StringBuilder sb = new StringBuilder();
-		String ttbkey = "ttbgksxodur11530001";
-		int maxResults = 10;
-		
-		sb.append("http://www.aladin.co.kr/ttb/api/ItemSearch.aspx");
-		sb.append("?ttbkey=");
-		sb.append(ttbkey);
-		sb.append("&Query=" + keyword);
-		sb.append("&MaxResults=");
-		sb.append(maxResults);
-		sb.append("&start=1&SearchTarget=Book&output=xml&Version=20131101");
-		
-		log.info("url : {}",sb);
-		log.info("keyword : {}", keyword);
-		
 	
-        
-		
-		return responseData;
+	@GetMapping
+	public String insertBook() {
+
+		return "book/insertBook";
 	}
 }
